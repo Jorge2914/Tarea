7175ffca9b5f735905f32d659fc6b8ab913ea946
Jorge2914 <jorgitirunxd@gmail.com>
2024-10-12 22:45:10 -0600

    tarea

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
-
-const b = "hola"
-const a = "emilio"
-
-function suma (a, b) {
-  const resultado = `${a} ${b}, como estas ${b}`
-  return console.log(resultado)
-}
-
-suma(b,a )
-{}
-
 /* 1. Suma de tres números
 Escribe una función que tome tres números como entrada y devuelva la suma de esos números.
 
@@ -19,7 +7,6 @@ Tres números (por ejemplo: 4, 7, 10)
 Salida esperada:
 
 La suma de esos tres números.
-
 
 2. Promedio de cuatro números
 Crea una función que tome cuatro números y calcule el promedio de ellos.
@@ -42,3 +29,35 @@ Salida esperada:
 
 La cantidad equivalente en euros. 
 */
+
+let a = 4
+let b = 7
+let c = 10
+
+function suma(a, b, c) {
+  const resultado2 = a + b + c
+    return console.log(resultado2)
+}
+
+suma(a, b, c)
+
+let d = 12
+let e = 15
+let f = 18
+let g = 22
+function promedio (d, e, f, g) {
+  const resultado6 = d + e + f + g
+  const resultado7 = resultado6 / 4
+  return console.log(resultado7)
+}
+
+promedio(12, 15, 18, 22)
+
+const dolar = 100
+const euro = 0.85 
+
+function conversor (dolar, euro) {
+  const resultado8 = dolar * euro
+  return console.log(resultado8)
+}
+conversor(euro, dolar)
